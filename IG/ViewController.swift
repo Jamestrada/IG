d630697e547b46bd72294dd963874619043125b1
James Estrada <Jamestrada@users.noreply.github.com>
2021-05-07 17:02:57 -0600

    Initial Commit

---
+++ IG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IG
+//
+//  Created by James Estrada on 5/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
