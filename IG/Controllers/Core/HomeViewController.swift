96cf018cd5647feb7f89dc53c9769cf816b76597
James Estrada <Jamestrada@users.noreply.github.com>
2021-05-07 18:02:11 -0600

    Initial Commit

---
+++ IG/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IG
+//
+//  Created by James Estrada on 5/7/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
