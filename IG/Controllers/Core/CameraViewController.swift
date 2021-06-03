ea78f129b355f33156d1a24fffe567409c4ba31f
James Estrada <Jamestrada@users.noreply.github.com>
2021-06-02 23:44:18 -0600

    request camera access

--- IG/Controllers/Core/CameraViewController.swift
+++ IG/Controllers/Core/CameraViewController.swift
@@ -6,12 +6,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    private var output = AVCapturePhotoOutput()
+    private var captureSession: AVCaptureSession?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
+        setUpNavBar()
+        checkCameraPermission()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tabBarController?.tabBar.isHidden = true // Hide tab bar when selecting the camera tab
         
+    }
+    
+    private func checkCameraPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
+        case .notDetermined:
+            // Request access
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.setUpCamera()
+                }
+            }
+        case .authorized:
+            setUpCamera()
+        case .restricted:
+            break
+        case .denied:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    private func setUpCamera() {
+        let captureSession = AVCaptureSession()
         
+        // Add device
+    }
+    
+    @objc func didTapClose() {
+        tabBarController?.selectedIndex = 0
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    private func setUpNavBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapClose))
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage() // hide navbar divider
+        navigationController?.navigationBar.backgroundColor = .clear
     }
 }
