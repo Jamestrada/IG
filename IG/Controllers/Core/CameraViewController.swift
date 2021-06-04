8f3bbcf36bf78aa39d6935639d00b533a347aa86
James Estrada <Jamestrada@users.noreply.github.com>
2021-06-03 23:54:48 -0600

    create custom camera

--- IG/Controllers/Core/CameraViewController.swift
+++ IG/Controllers/Core/CameraViewController.swift
@@ -12,10 +12,12 @@ class CameraViewController: UIViewController {
     
     private var output = AVCapturePhotoOutput()
     private var captureSession: AVCaptureSession?
+    private let previewLayer = AVCaptureVideoPreviewLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        title = "Take Photo"
         setUpNavBar()
         checkCameraPermission()
     }
@@ -23,7 +25,19 @@ class CameraViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         tabBarController?.tabBar.isHidden = true // Hide tab bar when selecting the camera tab
-        
+        if let session = captureSession, !session.isRunning { // resume camera
+            session.startRunning()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession?.stopRunning() // pause camera from running in the background
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.width, height: view.width)
     }
     
     private func checkCameraPermission() {
@@ -53,6 +67,27 @@ class CameraViewController: UIViewController {
         let captureSession = AVCaptureSession()
         
         // Add device
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if captureSession.canAddInput(input) { // makes sure that there won't be conflicts with other inputs
+                    captureSession.addInput(input)
+                }
+            } catch {
+                print(error)
+            }
+            
+            if captureSession.canAddOutput(output) {
+                captureSession.addOutput(output)
+            }
+            
+            // Layer
+            previewLayer.session = captureSession
+            previewLayer.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(previewLayer)
+            
+            captureSession.startRunning()
+        }
     }
     
     @objc func didTapClose() {
@@ -62,8 +97,9 @@ class CameraViewController: UIViewController {
     
     private func setUpNavBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapClose))
-        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        navigationController?.navigationBar.shadowImage = UIImage() // hide navbar divider
-        navigationController?.navigationBar.backgroundColor = .clear
+        // make navbar transparent
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+//        navigationController?.navigationBar.shadowImage = UIImage() // hide navbar divider
+//        navigationController?.navigationBar.backgroundColor = .clear
     }
 }
