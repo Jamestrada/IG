3a943e16879c91058d338d9443c044a5527d44ab
James Estrada <Jamestrada@users.noreply.github.com>
2021-06-15 22:17:19 -0600

    add search for users functionality

--- IG/Controllers/Core/ExploreViewController.swift
+++ IG/Controllers/Core/ExploreViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ExploreViewController: UIViewController {
+class ExploreViewController: UIViewController, UISearchResultsUpdating {
     
     private let searchVC = UISearchController(searchResultsController: SearchResultsViewController())
 
@@ -15,7 +15,29 @@ class ExploreViewController: UIViewController {
         super.viewDidLoad()
         title = "Explore"
         view.backgroundColor = .systemBackground
+        (searchVC.searchResultsController as? SearchResultsViewController)?.delegate = self
         searchVC.searchBar.placeholder = "Search..."
+        searchVC.searchResultsUpdater = self
         navigationItem.searchController = searchVC
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        DatabaseManager.shared.findUsers(with: query) { results in
+            DispatchQueue.main.async {
+                resultsVC.update(with: results)
+            }
+        }
+    }
+}
+
+extension ExploreViewController: SearchResultsViewControllerDelegate {
+    func searchResultsViewController(_ VC: SearchResultsViewController, didSelectResultWith user: User) {
+        let vc = ProfileViewController(user: user)
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
