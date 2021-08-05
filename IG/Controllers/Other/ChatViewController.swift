9aef39a83dffa12b1fd23dbd92dd899647fc25f8
James Estrada <Jamestrada@users.noreply.github.com>
2021-08-04 23:19:25 -0600

    build chat interface

--- IG/Controllers/Other/ChatViewController.swift
+++ IG/Controllers/Other/ChatViewController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import MessageKit
 
-class ChatViewController: UIViewController {
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
+
+struct Sender: SenderType {
+    var senderId: String
+    var displayName: String
+    var photoURL: String
+}
+
+class ChatViewController: MessagesViewController {
+    
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(senderId: "1", displayName: "James Estrada", photoURL: "")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("What's up from IG")))
+        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("What's up from IG. What's up from IG. What's up from IG. What's up from IG")))
         view.backgroundColor = .red
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
     }
 
+}
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType { // The framework shows the chat bubble on the right for sender and left for receiver
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
 }
